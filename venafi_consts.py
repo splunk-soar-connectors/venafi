85dc949da4737eac9917837e34e21c5001e29375
Ricardo Munoz <rmunoz@rmunoz-mbp-9d822.sv.splunk.com>
2019-04-08 11:12:43 -0700

    deleted unused file

--- venafi_consts.py
+++
@@ -1,8 +0,0 @@
-# File: venafi_consts.py
-# Copyright (c) 2019 Splunk Inc.
-#
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
-#
-#
-# --
