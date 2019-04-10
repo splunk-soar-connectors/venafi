66dcb9aaa1165ede36d89b378d496bc0a94f3d1b
Jay Patel <jay.patel@M06-JayP.local>
2019-04-10 13:28:07 +0530

    Venafi: Fixing the copyrights year and unicode in copyrights template in __init__.py file

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@
 #
 # Copyright (c) 2019 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
 # --
