0afb49d43c068695f46a4f40e6c63b8503d5c49f
pdesai-crest <pdesai@splunk.com>
2021-08-04 17:41:10 +0530

    consts file

---
+++ venafi_consts.py
@@ -0,0 +1,5 @@
+# File: venafi_consts.py
+# Copyright (c) 2019-2021 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
